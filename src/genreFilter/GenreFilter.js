0ee8c6d27faa71f87a5dd1d512211a361e8c83e4
Shivammaurya011 <shivammaurya011@gmail.com>
2023-03-18 19:50:29 +0530

    initial commit

--- src/genreFilter/GenreFilter.js
+++ src/genreFilter/GenreFilter.js
@@ -8,10 +8,9 @@ function GenreFilter({arr}) {
       <h2>Filter by Genre</h2>
       <div className='btn'>
         {arr.map((data, i)=>{
-          {return (
+          return (
             <button key={i} onClick={(e) => console.log(e.target.__reactProps$mp5id9cvumd.children)}>{data}</button>
           )
-          }
         })}
       </div>
     </div>
